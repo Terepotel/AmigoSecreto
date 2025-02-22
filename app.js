b6e32a3aeb09aec43d9d18ddba6037a0b579c2dc
Teresa Potel <terepotel13@gmail.com>
2025-02-22 11:00:51 -0300

    limpieza de codigo

--- app.js
+++ app.js
@@ -3,27 +3,23 @@ let elementoLista = document.querySelector("#listaAmigos");
 let resultado = document.querySelector("#resultado");
 
 function agregarAmigo() {
-  //captura el nombe ingresado
+
   let inputNombre = document.getElementById("amigo").value;
-  console.log(inputNombre);
 
-  //verifica que haya un nombre
+
   if (inputNombre.length > 0) {
-    listaNombres.push(inputNombre); //se ingresa el nombre a la lista
+    listaNombres.push(inputNombre); 
   } else {
-    alert("ingrese un nombre"); //alerta de que no ingreso el nombre
+    alert("ingrese un nombre"); 
   }
-
-  console.log(listaNombres);
-  //mostrar lista de nombres
-  limpiarCaja(); //se llama a la funcion para cuando se ingresa el nombre del amigo a la liSTA se limpie el cuadro del input
+  limpiarCaja(); 
 
   crearListaAmigos();
 }
 
-//funcion para mostrar nombres de la lista
+
 function crearListaAmigos() {
-  // limpia el Ul antes de empezar a iterar
+
   resultado.innerHTML = "";
   elementoLista.innerHTML = "";
   for (let i = 0; listaNombres.length > i; i++) {
@@ -31,11 +27,9 @@ function crearListaAmigos() {
   }
 }
 
-//funcion para sortear y mostrar el elegido
 function sortearAmigo() {
   if (listaNombres.length > 1) {
     let amigoSorteado = Math.floor(Math.random() * listaNombres.length);
-    console.log(listaNombres[amigoSorteado]);
     elementoLista.innerHTML = "";
     resultado.innerHTML = `<li>${listaNombres[amigoSorteado]}</li>`;
     listaNombres = [];
@@ -44,7 +38,7 @@ function sortearAmigo() {
   }
 }
 
-//limpia la caja del imput
+
 function limpiarCaja() {
   let valorCaja = document.querySelector("#amigo");
   valorCaja.value = "";
